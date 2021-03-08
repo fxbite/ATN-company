a4893015d824e11ab13701daf472a13cd6b2cf1c
fxbite <letruongthinh1703@gmail.com>
2021-03-08 10:56:45 +0700

    Add source code

---
+++ connect.php
@@ -0,0 +1,19 @@
+<?php
+
+$host = "ec2-54-159-175-113.compute-1.amazonaws.com";
+$db = "d54jga5rdkh2ad";
+$user = "exggdztndbdebf";
+$port = "5432";
+$pass = "7ae7d3acef3d6c224a91e44039fa8097fc84587aa9192914201bc6e05af40f41";
+$ssl = "require";
+
+$conn = pg_connect("dbname=$db host=$host port=$port user=$user password=$pass sslmode=$ssl");
+
+if (!$conn) {
+    echo "Database connection failed.";
+} else {
+     echo "Database connection success.";
+}
+
+
+?>
