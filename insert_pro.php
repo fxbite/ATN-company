b39dde8b3e4f7ad3a7fd5cf071ae7d53228d6621
Fxbite <letruongthinh1703@gmail.com>
2021-03-11 13:19:39 +0700

    Test 12

--- insert_pro.php
+++ insert_pro.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -9,4 +9,8 @@
 <body>
     <h1>hello</h1>
 </body>
-</html>
+</html> -->
+
+<?php
+echo "Database";
+?>
