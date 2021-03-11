0b444cdd56d36f6e6dc563f6b9a6bccd5767f581
Fxbite <letruongthinh1703@gmail.com>
2021-03-11 13:37:29 +0700

    insert data

--- connect.php
+++ connect.php
@@ -18,15 +18,7 @@ if (!$con) {
    // return $con;
 
 }
-// $idCust = $_POST["id_cust"];
-// $custName = $_POST["name"];
-// $custPhone = $_POST["phone"];
-// $custEmail = $_POST["email"];
 
-// echo $idCust; 
-// echo $custName; 
-// echo $custPhone; 
-// echo $custEmail;
     
 // $sql = "INSERT INTO customer (idCust, nameCust, phone, email) VALUES 
 //         ('$idCust', '$custName', '$custPhone', '$custEmail')";
--- insert_buy.php
+++ insert_buy.php
@@ -2,6 +2,16 @@
 require_once "connect.php";
 
 $conn = connectdb();
+
+$idCust = $_POST["id_cust"];
+$custName = $_POST["name"];
+$custPhone = $_POST["phone"];
+$custEmail = $_POST["email"];
+
+echo $idCust; 
+echo $custName; 
+echo $custPhone; 
+echo $custEmail;
 ?>
 
 
