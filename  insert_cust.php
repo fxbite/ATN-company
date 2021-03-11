2bb9b74f64789b903f59e80f27a21c9bc9277093
Fxbite <letruongthinh1703@gmail.com>
2021-03-11 13:27:06 +0700

    test23

---  insert_cust.php
+++  insert_cust.php
@@ -1,31 +1,7 @@
 <?php
-// require_once "connect.php";
+
 echo "Database";
-// $conn = connectdb();
 ?>
-
-        <!-- // $link = connectDB();
-        // // get data from FORM
-        // $idCust = $_POST['id_cust'];
-        // $custName = $_POST['name'];
-        // $custPhone = $_POST['phone'];
-        // $custEmail = $_POST['email'];
-
-        // echo $idCust; <br>
-        // echo $custName; <br>
-        // echo $custPhone; <br>
-        // echo $custEmail; <br>
-        // $sql = "INSERT INTO customer (idCust, nameCust, phone, email) VALUES 
-        //     ('$idCust', '$custName', '$custPhone', '$custEmail')";
-
-        // echo $sql; <br>
-
-        // if (pg_query($link,$sql)){
-        //     echo "Records added successfully.";
-        // } else {
-        //     echo "Records failed." . pg_error($conn);
-        // } -->
-
 
 
 
