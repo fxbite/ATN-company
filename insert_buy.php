cfe8caac5324b7d66d4828c7412eed43e611640b
Fxbite <letruongthinh1703@gmail.com>
2021-03-11 17:26:51 +0700

    change insert method

--- insert_buy.php
+++ insert_buy.php
@@ -3,25 +3,25 @@ require_once "connect.php";
 
 $conn = connectdb();
 
-$idCust = $_POST["id_cust"];
-$custName = $_POST["name"];
-$custPhone = $_POST["phone"];
-$custEmail = $_POST["email"];
+// $idCust = $_POST["id_cust"];
+// $custName = $_POST["name"];
+// $custPhone = $_POST["phone"];
+// $custEmail = $_POST["email"];
 
-echo $idCust; 
-echo $custName; 
-echo $custPhone; 
-echo $custEmail;
+// echo $idCust; 
+// echo $custName; 
+// echo $custPhone; 
+// echo $custEmail;
 
 // $sql = "INSERT INTO customer (idCust, nameCust, phone, email) VALUES 
 //         ('$idCust', '$custName', '$custPhone', '$custEmail')";
 
-$query = "INSERT INTO customer VALUES ('$_POST[id_cust]','$_POST[name]',
+$sql = "INSERT INTO customer VALUES ('$_POST[id_cust]','$_POST[name]',
 '$_POST[phone]','$_POST[email]')";
 
 echo $query;
 
-if (pg_query($conn,$query)){
+if (pg_query($conn,$sql)){
     echo "Records added successfully.";
 } else {
     echo "Records failed.";
