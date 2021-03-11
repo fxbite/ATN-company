bf006d424b94f69d6eafe2c80a91880056ba19ec
fxbite <letruongthinh1703@gmail.com>
2021-03-11 10:14:28 +0700

    Fix 2

--- connect.php
+++ connect.php
@@ -15,24 +15,24 @@
     }
     
 
-    $idCust = $_POST["id_cust"];
-        $custName = $_POST["name"];
-        $custPhone = $_POST["phone"];
-        $custEmail = $_POST["email"];
+    // $idCust = $_POST["id_cust"];
+    //     $custName = $_POST["name"];
+    //     $custPhone = $_POST["phone"];
+    //     $custEmail = $_POST["email"];
 
-        echo $idCust; <br>
-        echo $custName; <br>
-        echo $custPhone; <br>
-        echo $custEmail; <br>
-        $sql = "INSERT INTO customer (idCust, nameCust, phone, email) VALUES 
-            ('$idCust', '$custName', '$custPhone', '$custEmail')";
+    //     echo $idCust; <br>
+    //     echo $custName; <br>
+    //     echo $custPhone; <br>
+    //     echo $custEmail; <br>
+    //     $sql = "INSERT INTO customer (idCust, nameCust, phone, email) VALUES 
+    //         ('$idCust', '$custName', '$custPhone', '$custEmail')";
 
-        echo $sql; <br>
+    //     echo $sql; <br>
 
-        if (pg_query($conn,$sql)){
-            echo "Records added successfully.";
-        } else {
-            echo "Records failed." . pg_error($conn);
-        }
+    //     if (pg_query($conn,$sql)){
+    //         echo "Records added successfully.";
+    //     } else {
+    //         echo "Records failed." . pg_error($conn);
+    //     }
 
 ?>
