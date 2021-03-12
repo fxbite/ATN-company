c86627ad39fcef6302ad073b6acc70e960d4c1d6
fxbite <fxbite@DESKTOP-FRKNOAK.localdomain>
2021-03-12 11:01:40 +0700

    Modified insert 2

--- insert_pro.php
+++ insert_pro.php
@@ -1,16 +1,14 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>hello</h1>
-</body>
-</html> -->
-
 <?php
-echo "Database";
+require_once "connect.php";
+
+$conn = connectdb();
+
+$sql = "INSERT INTO product VALUES ('$_POST[id_cust]','$_POST[name]',
+'$_POST[phone]','$_POST[email]')";
+
+if (pg_query($conn,$sql)){
+    echo "Records added successfully.";
+} else {
+    echo "Records failed.";
+}  
 ?>
