da60cfd7291d4a486c529fb0aebaa1532d5a9c67
fxbite <letruonghthinh1703@gmail.com>
2021-03-13 08:51:41 +0700

    Fix URL index

---
+++ nav.php
@@ -0,0 +1,14 @@
+<?php
+ <nav class="navbar navbar-dark navbar-expand-lg fixed-top bg-white portfolio-navbar gradient">
+ <div class="container"><a class="navbar-brand logo" href="#">Shoptoy365</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navbarNav"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+     <div class="collapse navbar-collapse"
+         id="navbarNav">
+         <ul class="nav navbar-nav ml-auto">
+             <li class="nav-item" role="presentation"><a class="nav-link active" href="index.php">Home</a></li>
+             <li class="nav-item" role="presentation"><a class="nav-link" href="products.php">Products</a></li>
+             <li class="nav-item" role="presentation"><a class="nav-link active" href="project-grid-cards.html">Contact</a></li>
+         </ul>
+     </div>
+ </div>
+</nav>
+?>
