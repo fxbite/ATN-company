f0a0ee031aa405bf0a8d06b1811b58070e2484d7
fxbite <letruonghthinh1703@gmail.com>
2021-03-13 13:12:04 +0700

    Add varibale

--- insert_buy.php
+++ insert_buy.php
@@ -1,10 +1,26 @@
 <?php
 require_once "connect.php";
 
-$conn = connectdb();
 
-$sql = "INSERT INTO customer VALUES ('$_POST[id_cust]','$_POST[name]',
-'$_POST[phone]','$_POST[email]')";
+if (isset($_POST['submit'])) {
+    $conn = connectdb();
+
+     // get data from FORM
+    $idBuy = $_POST['id_cust'];
+    $nameBuy = $_POST['name'];
+    $phoneBuy = $_POST['phone'];
+    $emailBuy = $_POST['email'];
+
+    echo $idBuy;
+    echo $nameBuy;
+    echo $phoneBuy;
+    echo $emailBuy;
+}
+    $sql = "INSERT INTO customer VALUES ('$idBuy',' $nameBuy',
+    ' $phoneBuy','$emailBuy')";
+
+    // $sql = "INSERT INTO customer VALUES ('$_POST[id_cust]','$_POST[name]',
+    // '$_POST[phone]','$_POST[email]')";
 
 if (pg_query($conn,$sql)){
     echo "Records added successfully.";
