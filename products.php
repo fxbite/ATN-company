5c0ec890e1a8edbfe876cfc74d43868a08bea860
fxbite <letruonghthinh1703@gmail.com>
2021-03-13 17:55:55 +0700

    check form product

--- products.php
+++ products.php
@@ -40,53 +40,30 @@
 	<body>
 	<div class="container" style="width: 65%">
         <h2>Shopping Cart</h2>
-        <?php
-		    require_once 'connect.php';
-			$conn = connectdb();
-            $query = "SELECT * FROM product ORDER BY idPro ASC ";
-            $result = pg_query($conn,$query);
-            if(pg_num_rows($result) > 0) {
+      
+		    // require_once 'connect.php';
+			// $conn=connectDB();
+            // $sql = "select * from product";
 
-                while ($row = pg_fetch_array($result)) {
+            // $products= mysqli_query($conn, $sql);
+            // $i= 9;
+            // $count = 0;
+            // while ($count < $i && $row = mysqli_fetch_array($products)) { 
+            // $count++;
+			<div class="col-md-3">
+                <form method="post" action="Cart.php?action=add&id=<?php echo $row["id"]; ?>">
 
-                    ?>
-                    <div class="col-md-3">
-
-                        <form method="POST" action="products.php?action=add&id=<?php echo $row['idPro']; ?>">
-
-                            <div class="product">
-                                <img src="<?php echo $row['image']; ?>" class="img-responsive">
-                                <h5 class="text-info"><?php echo $row['namePro']; ?></h5>
-                                <h5 class="text-danger"><?php echo $row['price']; ?></h5>
-                                <input type="text" name="quantity" class="form-control" value="1">
-                                <input type="hidden" name="hidden_name" value="<?php echo $row['namePro']; ?>">
-                                <input type="hidden" name="hidden_price" value="<?php echo $row['price']; ?>">
-                                <input type="submit" name="add" style="margin-top: 5px;" class="btn btn-success"
-                                       value="Add to Cart">
-                            </div>
-                        </form>
+                    <div class="product">
+                        <img src="<?php echo $row["image"]; ?>" class="img-responsive">
+                        <h5 class="text-info"><?php echo $row["pname"]; ?></h5>
+                        <h5 class="text-danger"><?php echo $row["price"]; ?></h5>
+                        <input type="text" name="quantity" class="form-control" value="1">
+                        <input type="hidden" name="hidden_name" value="<?php echo $row["pname"]; ?>">
+                        <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>">
+                        <input type="submit" name="add" style="margin-top: 5px;" class="btn btn-success"
+                            value="Add to Cart">
                     </div>
-                    <?php
-                }
-            }
-        ?>
-
-        
-    </div>
-
+                </form>
+            </div>
 	</body>
 </html>
-
-<?php
-//If you have use Older PHP Version, Please Uncomment this function for removing error 
-
-/*function array_column($array, $column_name)
-{
-	$output = array();
-	foreach($array as $keys => $values)
-	{
-		$output[] = $values[$column_name];
-	}
-	return $output;
-}*/
-?>
