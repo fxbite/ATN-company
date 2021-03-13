2a713c33d04e075492102dee15787033870a4873
fxbite <letruonghthinh1703@gmail.com>
2021-03-13 17:41:09 +0700

    test demo

--- products.php
+++ products.php
@@ -1,48 +1,3 @@
-<?php
-    session_start();
-    require_once 'connect.php';
-	$conn = connectdb();
-
-    if (isset($_POST['add'])){
-        if (isset($_SESSION['cart'])){
-            $item_array_id = array_column($_SESSION['cart'],'product_id');
-            if (!in_array($_GET["id"],$item_array_id)){
-                $count = count($_SESSION["cart"]);
-                $item_array = array(
-                    'product_id' => $_GET["id"],
-                    'item_name' => $_POST["hidden_name"],
-                    'product_price' => $_POST["hidden_price"],
-                    'item_quantity' => $_POST["quantity"],
-                );
-                $_SESSION["cart"][$count] = $item_array;
-                echo '<script>window.location="products.php"</script>';
-            }else{
-                echo '<script>alert("Product is already Added to Cart")</script>';
-                echo '<script>window.location="products.php"</script>';
-            }
-        }else{
-            $item_array = array(
-                'product_id' => $_GET["id"],
-                'item_name' => $_POST["hidden_name"],
-                'product_price' => $_POST["hidden_price"],
-                'item_quantity' => $_POST["quantity"],
-            );
-            $_SESSION["cart"][0] = $item_array;
-        }
-    }
-
-    if (isset($_GET["action"])){
-        if ($_GET["action"] == "delete"){
-            foreach ($_SESSION["cart"] as $keys => $value){
-                if ($value["product_id"] == $_GET["id"]){
-                    unset($_SESSION["cart"][$keys]);
-                    echo '<script>alert("Product has been Removed...!")</script>';
-                    echo '<script>window.location="products.php"</script>';
-                }
-            }
-        }
-    }
-?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -86,6 +41,8 @@
 	<div class="container" style="width: 65%">
         <h2>Shopping Cart</h2>
         <?php
+		    require_once 'connect.php';
+			$conn = connectdb();
             $query = "SELECT * FROM product ORDER BY idPro ASC ";
             $result = pg_query($conn,$query);
             if(pg_num_rows($result) > 0) {
@@ -114,48 +71,7 @@
             }
         ?>
 
-        <div style="clear: both"></div>
-        <h3 class="title2">Shopping Cart Details</h3>
-        <div class="table-responsive">
-            <table class="table table-bordered">
-            <tr>
-                <th width="30%">Product Name</th>
-                <th width="10%">Quantity</th>
-                <th width="13%">Price Details</th>
-                <th width="10%">Total Price</th>
-                <th width="17%">Remove Item</th>
-            </tr>
-
-            <?php
-                if(!empty($_SESSION['cart'])){
-                    $total = 0;
-                    foreach ($_SESSION['cart'] as $key => $value) {
-                        ?>
-                        <tr>
-                            <td><?php echo $value['item_name']; ?></td>
-                            <td><?php echo $value['item_quantity']; ?></td>
-                            <td>$ <?php echo $value['product_price']; ?></td>
-                            <td>
-                                $ <?php echo number_format($value['item_quantity'] * $value['product_price'], 2); ?></td>
-                            <td><a href="Cart.php?action=delete&id=<?php echo $value['product_id']; ?>"><span
-                                        class="text-danger">Remove Item</span></a></td>
-
-                        </tr>
-                        <?php
-                        $total = $total + ($value['item_quantity'] * $value['product_price']);
-                    }
-                        ?>
-                        <tr>
-                            <td colspan="3" align="right">Total</td>
-                            <th align="right">$ <?php echo number_format($total, 2); ?></th>
-                            <td></td>
-                        </tr>
-                        <?php
-                    }
-                ?>
-            </table>
-        </div>
-
+        
     </div>
 
 	</body>
