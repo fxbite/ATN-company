9840bbdecf0eda9ec4c9134520939d61e614fc45
fxbite <letruonghthinh1703@gmail.com>
2021-03-13 18:03:47 +0700

    Add connect database to form pro

--- products.php
+++ products.php
@@ -40,30 +40,32 @@
 	<body>
 	<div class="container" style="width: 65%">
         <h2>Shopping Cart</h2>
-      
-		    <!-- // require_once 'connect.php';
-			// $conn=connectDB();
-            // $sql = "select * from product";
+           <?php
+		    require_once 'connect.php';
+			$conn=connectDB();
+            $sql = "select * from product";
 
-            // $products= mysqli_query($conn, $sql);
-            // $i= 9;
-            // $count = 0;
-            // while ($count < $i && $row = mysqli_fetch_array($products)) { 
-            // $count++; -->
+            $products= mysqli_query($conn, $sql);
+            $i= 9;
+            $count = 0;
+            while ($count < $i && $row = mysqli_fetch_array($products)) { 
+            $count++;
+			?>
 			<div class="col-md-3">
-                <form method="post" action="Cart.php?action=add&id=<?php echo $row["id"]; ?>">
+                <form method="post" action="Cart.php?action=add&id=<?php echo $row["idPro"]; ?>">
 
                     <div class="product">
                         <img src="<?php echo $row["image"]; ?>" class="img-responsive">
-                        <h5 class="text-info"><?php echo $row["pname"]; ?></h5>
+                        <h5 class="text-info"><?php echo $row["namePro"]; ?></h5>
                         <h5 class="text-danger"><?php echo $row["price"]; ?></h5>
                         <input type="text" name="quantity" class="form-control" value="1">
-                        <input type="hidden" name="hidden_name" value="<?php echo $row["pname"]; ?>">
+                        <input type="hidden" name="hidden_name" value="<?php echo $row["namePro"]; ?>">
                         <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>">
                         <input type="submit" name="add" style="margin-top: 5px;" class="btn btn-success"
                             value="Add to Cart">
                     </div>
                 </form>
             </div>
+			<?php } ?>
 	</body>
 </html>
