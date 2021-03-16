bbe765d2f49294d5333e49e56fe1c0950a5a48ec
fxbite <letruonghthinh1703@gmail.com>
2021-03-16 18:18:17 +0700

    upload order_detail

--- products.php
+++ products.php
@@ -52,16 +52,16 @@
             $count++;
 			?>
 			<div class="col-md-3">
-                <form method="post" action="products.php?action=add&id=<?php echo $row["idPro"]; ?>">
+                <form method="post" action="order_detail.php?action=add&id=<?php echo $row["idPro"]; ?>">
 
                     <div class="product">
                         <img src="assets/img/data/<?php echo $row["image"]; ?>" class="img-responsive">
                         <h5 class="text-info"><?php echo $row["namePro"]; ?></h5>
-                        <h5 class="text-danger"><?php echo $row["price"]; ?></h5>
+                        <h5 class="text-danger">$ <?php echo $row["price"]; ?></h5>
                         <input type="text" name="quantity" class="form-control" value="1">
                         <input type="hidden" name="hidden_name" value="<?php echo $row["namePro"]; ?>">
                         <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>">
-                        <input type="submit" name="add" style="margin-top: 5px;" class="btn btn-success"
+                        <input type="submit" name="add_to_cart" style="margin-top: 5px;" class="btn btn-success"
                             value="Add to Cart">
                     </div>
                 </form>
