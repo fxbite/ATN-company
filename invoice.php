f779a6cfc13efb8c7f6e2cb188afafed1fbb9769
Fxbite <letruongthinh1703@gmail.com>
2021-03-18 06:25:51 +0700

    test invoice function

--- invoice.php
+++ invoice.php
@@ -1,15 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
 <?php 
-  echo 'test';
+require_once 'connect.php';
+require 'products.php';
+
+$con = connectdb();
+
+// Get data from save invoice form
+$phone_num = $_POST['phone_cust'];
+
+// Search ID customer
+$query = "select * from customer where \"phone\"= '$phone_num'";
+$info = pg_query($con, $query);
+$row = pg_fetch_array($info);
+
+echo $row;
+
+
 ?>
-    
-</body>
-</html>
