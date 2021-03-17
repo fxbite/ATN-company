0e3f067a3bed8e25da25ed47c0adb7bc18d84d4d
Fxbite <letruongthinh1703@gmail.com>
2021-03-18 06:05:33 +0700

    test invoice submit

--- invoice.php
+++ invoice.php
@@ -1,21 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php 
-require_once "connect.php";
-$con = connectdb();
-
-// Get info from Save Invoice form 
-$name_cust = $_POST['name_cust'];
-$phone_cust = $_POST['phone_cust'];
-
-// Get info from customer table
-$sql = "select * from customer";
-$check = pg_query($con, $sql);
-$row = pg_fetch_array($check);
-  
-if ($name_cust == $row['name_cust'] || $phone_cust == $row['phone']) {
-    $querry = "select * from customer where \"name_cust\" = '$name_cust' or \"phone\" = '$phone_cust'";
-    $get_data = pg_query($con, $querry);
-    $product = pg_fetch_array($get_data);
-    $query = "INSERT INTO invoice (id_cust, name_product, total) VALUES ('$product 
-}
-
+  echo 'test';
 ?>
+    
+</body>
+</html>
