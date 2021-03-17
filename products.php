6ba6bb21f9f8912478854cb067b53ed43c42780b
Fxbite <letruongthinh1703@gmail.com>
2021-03-17 14:22:02 +0700

    test query pro

--- products.php
+++ products.php
@@ -109,11 +109,11 @@ if(isset($_GET["action"]))
                 <form method="post" action="products.php?action=add&id=<?php echo $row["idPro"]; ?>">
 
                     <div class="product">
-                        <img src="assets/img/data/<?php echo $row["image"]; ?>" class="img-responsive">
-                        <h5 class="text-info"><?php echo $row["namePro"]; ?></h5>
+                        <img src="assets/img/data/<?php echo $row["image_pro"]; ?>" class="img-responsive">
+                        <h5 class="text-info"><?php echo $row["name_pro"]; ?></h5>
                         <h5 class="text-danger">$ <?php echo $row["price"]; ?></h5>
                         <input type="text" name="quantity" class="form-control" value="1">
-                        <input type="hidden" name="hidden_name" value="<?php echo $row["namePro"]; ?>">
+                        <input type="hidden" name="hidden_name" value="<?php echo $row["name_pro"]; ?>">
                         <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>">
                         <input type="submit" name="add_to_cart" style="margin-top: 5px;" class="btn btn-success"
                             value="Add to Cart">
