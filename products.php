438fb6fc3cfc79b26574f7e4ae96ad8a1069c299
Fxbite <letruongthinh1703@gmail.com>
2021-03-18 19:14:57 +0700

    test makeing up

--- products.php
+++ products.php
@@ -56,7 +56,6 @@ if(isset($_GET["action"]))
 <html>
 	<head>
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 		<title>Our products</title>
 		<style>
         @import url('https://fonts.googleapis.com/css?family=Titillium+Web');
