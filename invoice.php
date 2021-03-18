96904be09b114362937c7bd9c003e843a63ac35e
Fxbite <letruongthinh1703@gmail.com>
2021-03-18 07:47:33 +0700

    test echo12

--- invoice.php
+++ invoice.php
@@ -4,34 +4,28 @@ require_once 'connect.php';
 
 $con = connectdb();
 
-// Get data from save invoice form
+// Get data products.php
 $phone_num = $_POST['phone_cust'];
 $total_pro = $_SESSION["total"];
 $name_item = $_SESSION["item_name"];
-echo $total_pro;
-echo $name_item;
 
 
-// Search ID customer
+// Search vs Get ID customer
 $query = "select * from customer where \"phone\"= '$phone_num'";
 $info = pg_query($con, $query);
 $row = pg_fetch_array($info);
 
-// Get data from customer table
 $id_cust = $row['idCust'];
 
-// Get data from Order_detail Form
 
+// Search vs Get ID products
+$sql_query = "INSERT INTO product where \"name_pro\"= '$name_item'";
+$info1 = pg_query($con, $sql_query);
+$row1 = pg_fetch_array($info1);
 
-// // Search ID products
-// $sql_query = "INSERT INTO product where \"name_pro\"= '$name_item'";
-// $info1 = pg_query($con, $sql_query);
-// $row1 = pg_fetch_array($info1);
+$id_pro = $row1['idPro'];
+echo $id_pro;
 
-// // Get data from product table
-// $id_pro = $row1['idPro'];
-
-// echo $id_pro;
 
 
 // $insert_data = "INSERT INTO invoice (id_cust, id_pro, total) VALUES ('$id_cust',')";
